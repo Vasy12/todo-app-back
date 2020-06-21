cb38f61e1a1276a1b571b0f00d05b102839aff6b
Vasy Terkin <vasy.terkin12@gmail.com>
2020-06-21 22:32:36 +0300

    Add constants.js file with basic constants

---
+++ src/constants.js
@@ -0,0 +1,33 @@
+/**
+ * @typedef {(Symbol|string|number)} Action
+ * */
+/**
+ *
+ * @readonly
+ * @enum {Action}
+ */
+module.exports.ACTION = {
+  CREATE: Symbol('CREATE'),
+  READ: Symbol('READ'),
+  UPDATE: Symbol('UPDATE'),
+  DELETE: Symbol('DELETE'),
+  LOGIN: Symbol('LOGIN'),
+  SIGN_UP: Symbol('SIGN_UP'),
+};
+
+/**
+ * @typedef {(Symbol|string|number)} Entity
+ * */
+/**
+ * @readonly
+ * @enum {Entity}
+ */
+module.exports.ENTITY = {
+  USER: Symbol('USER'),
+  TASK: Symbol('TASK'),
+};
+
+module.exports.PASSWORD_RULE = {
+  pattern: /(?=.*?\d)(?=.*?[A-Z])(?=.*?[a-z])^{8,64}$/,
+  message: 'Your password must be at least 8 characters long, be of mixed case and also contain a digit or symbol. ',
+};

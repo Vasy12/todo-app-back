dbb06a547314e062aaaccf444e8bea01b5ca35ea
Vasy Terkin <vasy.terkin12@gmail.com>
2020-06-21 22:46:25 +0300

    Add Sequelize ORM setup

---
+++ config/sequelize.js
@@ -0,0 +1,11 @@
+module.exports = {
+  [process.env.NODE_ENV || 'development']: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    migrationStorage: 'json',
+    seederStorage: 'json',
+  },
+};

84122d1c53623c2aa694ba4a7ddd1705d2c2c100
Vasy Terkin <vasy.terkin12@gmail.com>
2020-07-03 00:50:13 +0300

    Fix password pattern error

--- src/constants.js
+++ src/constants.js
@@ -28,6 +28,6 @@ module.exports.ENTITY = {
 };
 
 module.exports.PASSWORD_RULE = {
-  PATTERN: /(?=.*?\d)(?=.*?[A-Z])(?=.*?[a-z])^{8,64}$/,
-  MESSAGE: 'Your password must be at least 8 characters long, be of mixed case and also contain a digit or symbol. ',
+    PATTERN: /(?=.*?\d)(?=.*?[A-Z])(?=.*?[a-z])^.{8,64}$/,
+    MESSAGE: 'Your password must be at least 8 characters long, be of mixed case and also contain a digit or symbol. ',
 };
